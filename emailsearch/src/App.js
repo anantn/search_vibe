b00ec69e6b271ee1624ad496126f92a17b166e53
Rukmani Ravi <lakhi.r@gmail.com>
2023-03-02 13:27:19 -0800

    search and display dummy results

--- emailsearch/src/App.js
+++ emailsearch/src/App.js
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 
-function App() {
-  const [searchResults, setSearchResults] = useState([]);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search_results: [], query: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.search = this.search.bind(this);
+  }
 
-  const handleSearch = searchTerm => {
-    // Use the search term to fetch search results
-    // and update the searchResults state
-    setSearchResults([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }]);
-  };
-
-  return (
-    <div>
-      <SearchBar onSearch={handleSearch} />
+  render() {
+    return (
+      <div>
+      <SearchBar onSearch={this.search} />
       {/* Render the search results */}
       <ul>
-        {searchResults.map(result => (
-          <li key={result.id}>{result.name}</li>
+        {this.state.search_results.map(result => (
+          <li key={result.id}>{result.text}</li>
         ))}
       </ul>
     </div>
-  );
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  search(query) {
+    if (query.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: query,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      search_results: state.search_results.concat(newItem),
+      query: ''
+    }));
+  }
+
 }
 
 export default App;
